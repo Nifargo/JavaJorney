26414e7ff90e8650d75295bdb042879561b5d135
DKV project <andrii.medvediev.ext@dkv-mobility.com>
2024-11-07 21:19:00 +0100

    first commit

---
+++ src/test/java/firstLessons/DifferentBetweenAll.java
@@ -0,0 +1,20 @@
+package firstLessons;
+
+public class DifferentBetweenAll {
+    public static void main(String[] args) {
+        String jvm = "Java Virtual Machine";
+        String jre = "Java Runtime Environment";
+        String jdk = "Java Development Kit";
+
+        String oop ="Basic concepts of OOPs are:\n" +
+                "\n" +
+                "Object\n" +
+                "Class\n" +
+                "Inheritance\n" +
+                "Polymorphism\n" +
+                "Abstraction\n" +
+                "Encapsulation";
+
+        System.out.println("Difference between JVM, JRE and JDK");
+    }
+}
